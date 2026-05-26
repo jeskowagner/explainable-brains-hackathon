4e95fab2e81b7d8cf3a96653491300d8afb8eb7d
JohannaPerens <johanna.perens@gmail.com>
2026-05-26 15:23:36 +0200

    Bucket access

--- bucket_access/config.py
+++ bucket_access/config.py
@@ -1,7 +1,7 @@
 # Configuration file for the Explainable Brains project.
 
-# Replace the X with your actual Hetzner Object Storage credentials and bucket information.
-HETZNER_ACCESS_KEY  = "X"
-HETZNER_SECRET_KEY  = "X"
+# Hetzner Object Storage credentials and bucket information
+HETZNER_ACCESS_KEY  = "L8J2M8NPTCLCFBPKJOKV"
+HETZNER_SECRET_KEY  = "Ztleuxih6189oVKYajI4aRJPaV0MdbcSx22Izl2W"
 HETZNER_BUCKET_NAME = "explainable-brains"
 HETZNER_ENDPOINT    = "https://fsn1.your-objectstorage.com"
